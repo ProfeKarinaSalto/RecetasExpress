1db146093fad270bc472cdd507eb7445ceeb7ffc
KSalto <karina.salto@sanjosempd.edu.ar>
2026-06-23 17:42:21 -0300

    Base creada por el docente

---
+++ script.js
@@ -0,0 +1,15 @@
+function guardarReceta(){
+
+var receta = document.getElementById(nombreReceta).value;
+
+if(receta == ""){
+
+alert("Debe ingresar una receta");
+
+}
+
+}
+
+function mostrarReceta(){
+
+}
